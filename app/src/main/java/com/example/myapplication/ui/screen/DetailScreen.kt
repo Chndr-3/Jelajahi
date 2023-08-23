d116508659adede82a42cbac0f7e32e1818d802f
Chandra <chandrawjy391@gmail.com>
2023-08-23 18:56:20 +0700

    Add gmaps feature to locate country

--- app/src/main/java/com/example/myapplication/ui/screen/DetailScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/screen/DetailScreen.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication.ui.screen
 
 
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -14,6 +16,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
@@ -28,7 +31,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.navigation.compose.rememberNavController
 import coil.compose.rememberAsyncImagePainter
 import com.example.jelajah.ui.viewmodel.MainViewModel
@@ -41,6 +46,9 @@ import com.example.myapplication.ui.theme.Purple80
 @Composable
 fun DetailScreen(viewModel: MainViewModel, navController: NavController) {
     val country = viewModel.country
+    val openUrlLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult()
+    ) {}
     Scaffold(topBar = {
         Surface(shadowElevation = 6.dp) {
             CenterAlignedTopAppBar(title = { Text("Detail Negara") }, navigationIcon = {
@@ -49,12 +57,25 @@ fun DetailScreen(viewModel: MainViewModel, navController: NavController) {
                 }
             })
         }
+    }, floatingActionButton = {
+        FloatingActionButton(onClick = {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(country?.maps?.googleMaps))
+            openUrlLauncher.launch(intent)
+        }) {
+            Icon(Icons.Filled.LocationOn, contentDescription = "Open Location")
+        }
     }) {
-        Column(verticalArrangement = Arrangement.Top, modifier = Modifier.padding(it).fillMaxSize()) {
+        Column(
+            verticalArrangement = Arrangement.Top, modifier = Modifier
+                .padding(it)
+                .fillMaxSize()
+        ) {
 
 
             Image(
-                painter = rememberAsyncImagePainter(country?.flags?.png ?: "https://flagcdn.com/w320/za.png"),
+                painter = rememberAsyncImagePainter(
+                    country?.flags?.png ?: "https://flagcdn.com/w320/za.png"
+                ),
                 contentScale = ContentScale.FillWidth,
                 contentDescription = null,
                 modifier = Modifier
@@ -62,27 +83,37 @@ fun DetailScreen(viewModel: MainViewModel, navController: NavController) {
             )
 
 
-                Column(Modifier.padding(10.dp).fillMaxWidth()) {
-                    Text(
-                        country?.name?.common ?: "Empty" ,
-                        style = TextStyle(
-                            fontSize = 30.sp,
-                            fontWeight = FontWeight.Bold,
-                            color = Purple40
-                        )
+            Column(
+                Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    country?.name?.common ?: "Empty",
+                    style = TextStyle(
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Purple40
                     )
-                    Text(
-                        "(${country?.name?.official ?: "Empty"})",
-                        style = TextStyle(
-                            fontSize = 16.sp,
-                            fontWeight = FontWeight.Bold,
-                            color = Purple80
-                        )
+                )
+                Text(
+                    "(${country?.name?.official ?: "Empty"})",
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Purple80
                     )
-                    Spacer(modifier = Modifier.height(20.dp))
-                    CountryInformationRow(label = "Region", value = country?.region ?: "Empty")
-                    CountryInformationRow(label = "Capital", value = country?.capital?.get(0) ?: "Empty")
-                    CountryInformationRow(label = "Population", value = formatWithCommas(country?.population ?: 0))
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+                CountryInformationRow(label = "Region", value = country?.region ?: "Empty")
+                CountryInformationRow(
+                    label = "Capital",
+                    value = country?.capital?.get(0) ?: "Empty"
+                )
+                CountryInformationRow(
+                    label = "Population",
+                    value = formatWithCommas(country?.population ?: 0)
+                )
             }
 
         }
@@ -91,13 +122,14 @@ fun DetailScreen(viewModel: MainViewModel, navController: NavController) {
 }
 
 @Composable
-fun CountryInformationRow(label: String, value : String){
-    Row (Modifier.fillMaxWidth(1f), horizontalArrangement = Arrangement.SpaceBetween){
+fun CountryInformationRow(label: String, value: String) {
+    Row(Modifier.fillMaxWidth(1f), horizontalArrangement = Arrangement.SpaceBetween) {
         Text(label, modifier = Modifier.weight(1.7f))
-        Text(":",modifier = Modifier.weight(0.5f))
-        Text(value,modifier = Modifier.weight(5f))
+        Text(":", modifier = Modifier.weight(0.5f))
+        Text(value, modifier = Modifier.weight(5f))
     }
 }
+
 @Preview
 @Composable
 fun MyComposablePreview() {
